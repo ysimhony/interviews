a29253562bc9284bff61d90c58e6e527b3aed012
Yacov Simhony <ysimhony@gmail.com>
2025-09-25 13:08:06 +0300

    Adding comment in the zombie_processes files to show how to detect zombie processes via the ps -xf command

--- volumez/zombie_process.c
+++ volumez/zombie_process.c
@@ -11,6 +11,24 @@ The zombie process remains in the process table:
 As long as the parent is alive and has not reaped it.
 
 Or until the parent exits, in which case the zombie is automatically reaped by the init process (PID 1).
+
+
+
+The zombie processes can be seen with the defunct keyword:
+
+yacovs@MOE-KT-6565923:~/interviews/volumez/shared_memory$ ps xf
+    PID TTY      STAT   TIME COMMAND
+    908 ?        Ss     0:00 ./wslconnect wsl -F -p 127.0.0.1:22004
+    909 pts/4    Ss     0:00  \_ -bash
+   1693 pts/4    R+     0:00      \_ ps xf
+    569 pts/1    S+     0:00 -bash
+    654 ?        Ss     0:00 ./wslconnect wsl -F -p 127.0.0.1:22003
+    655 pts/2    Ss     0:00  \_ -bash
+   1687 pts/2    S+     0:00      \_ ./zombie_process
+   1688 pts/2    Z+     0:00          \_ [zombie_process] <defunct> <--
+    545 ?        Ss     0:00 /usr/lib/systemd/systemd --user
+    546 ?        S      0:00  \_ (sd-pam)
+
 */
 int main() {
     pid_t pid = fork();
