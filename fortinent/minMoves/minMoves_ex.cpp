278f849f45526b3c65cb9e95afbe1a3a12195e07
Yacov Simhony <ysimhony@gmail.com>
2025-09-30 19:52:25 +0300

    Adding minveMoves_ex.cpp in order to exercise the minMoves chess question

---
+++ fortinent/minMoves/minMoves_ex.cpp
@@ -0,0 +1,12 @@
+
+int minMoves(int n, int startRow, int startCol, int endRow, int endCol)  {
+
+}
+
+int main() {
+    int res = minMoves(7, 6, 6, 0, 1);
+
+    cout << "result = " << res << endl;
+}
+
+
